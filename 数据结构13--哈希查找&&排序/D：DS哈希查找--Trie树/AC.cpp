6c87b3859284a6a09347cc6c28efc28999f7308e
Burppp <1483543906@qq.com>
2023-12-27 22:44:07 +0800

    update

--- 数据结构13--哈希查找&&排序/D：DS哈希查找--Trie树/AC.cpp
+++ 数据结构13--哈希查找&&排序/D：DS哈希查找--Trie树/AC.cpp
@@ -1,203 +1,114 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <queue>
+#include <string>
 using namespace std;
-class Binary_tree_node
+class trie;
+string str;
+string substr;
+queue<trie*> qe;
+class trie
 {
 public:
-	int data;
-	Binary_tree_node* left, * right;
-	Binary_tree_node() : left(NULL), right(NULL) {}
-	Binary_tree_node(int e) : data(e), left(NULL), right(NULL) {}
-	Binary_tree_node(Binary_tree_node*& external_root)
-	{
-		data = external_root->data;
-		left = external_root->left;
-		right = external_root->right;
-	}
+	char data;
+	vector<trie*>child;
+	trie(char ch) :data(ch) {}
+	void Insert();
+	void Insert_helper(trie* root, unsigned index);
+	void Level_traversal();
+	void Search();
+	void Search_helper(trie* root, unsigned index);
+	void DFS(trie* root, int& leaves);
 };
-class Binary_tree
+void trie::Insert_helper(trie* root, unsigned index)
 {
-public:
-	Binary_tree_node* root;
-	
-	Binary_tree() : root(NULL) {}
-	Binary_tree(int data)
-	{
-		root = new Binary_tree_node(data);
-	}
-	void Binary_tree_insert_helper(int data, Binary_tree_node*& external_root)
-	{
-		if (data > external_root->data)
-		{
-			if (external_root->right == NULL)
-			{
-				if(data != -1)
-					external_root->right = new Binary_tree_node(data);
-				else
-					external_root->right = NULL;
-				return;
-			}
-			else
-				Binary_tree_insert_helper(data, external_root->right);
-		}
-		else
-		{
-			if (external_root->left == NULL)
-			{
-				if(data != -1)
-					external_root->left = new Binary_tree_node(data);
-				else
-					external_root->left = NULL;
-				return;
-			}
-			else
-				Binary_tree_insert_helper(data, external_root->left);
-		}
+	int pos = 0;
+	if (index == str.size())
 		return;
-	}
-	void Binary_tree_insert(int data)
+	for (unsigned i = 0;i < root->child.size();i++)
 	{
-		if (root == NULL)
+		if (str[index] == root->child[i]->data)
 		{
-			root = new Binary_tree_node(data);
+			Insert_helper(root->child[i], index + 1);
 			return;
 		}
-		Binary_tree_insert_helper(data, root);
+		else if (root->child[i]->data < str[index])
+			pos++;
+	}
+	trie* insert_node = new trie(str[index]);
+	root->child.insert(root->child.begin() + pos, insert_node);
+	Insert_helper(root->child[root->child.size() - 1], index + 1);
+	return;
+}
+void trie::Insert()
+{
+	Insert_helper(this, 0);
+}
+void trie::Level_traversal()
+{
+	qe.push(this);
+	while (!qe.empty())
+	{
+		trie* cur_level = qe.front();
+		qe.pop();
+		if (cur_level->data != '#')
+			cout << cur_level->data;
+		for (unsigned i = 0;i < cur_level->child.size();i++)
+			qe.push(cur_level->child[i]);
+	}
+}
+void trie::DFS(trie* root, int& leaves)
+{
+	if (root->child.empty())
+		leaves++;
+	for (unsigned i = 0;i < root->child.size();i++)
+		DFS(root->child[i], leaves);
+}
+int flag = 0;
+void trie::Search_helper(trie* root, unsigned index)
+{
+	if (!root)
 		return;
-	}
-	void Binary_tree_inorder_traversal_helper(Binary_tree_node*& external_root, vector<int> &output)
-	{
-		if (external_root != NULL)
-		{
-			Binary_tree_inorder_traversal_helper(external_root->left, output);
-			//cout << external_root->data << ' ';
-			output.push_back(external_root->data);
-			Binary_tree_inorder_traversal_helper(external_root->right, output);
-		}
-	}
-	void Binary_tree_inorder_traversal(int k)
-	{
-		vector<int>output;
-		Binary_tree_inorder_traversal_helper(root, output);
-		//cout << endl;
-		cout << output[k] << endl;
-	}
-	void Binary_tree_delete_node_helper(int data, Binary_tree_node*& external_root)
-	{
-		if(external_root == NULL)
-			return;
-		if (data > external_root->data && external_root->right != NULL)
-		{
-			Binary_tree_delete_node_helper(data, external_root->right);
-		}
-		else if (data < external_root->data && external_root->left != NULL)
-		{
-			Binary_tree_delete_node_helper(data, external_root->left);
-		}
-		else if (data == external_root->data)
-		{
-			if (external_root->left == NULL && external_root->right == NULL)
-			{
-				delete external_root;
-				external_root = NULL;
-			}
-			else if (external_root->left != NULL && external_root->right == NULL)
-			{
-				external_root = external_root->left;
-				return;
-			}
-			else if (external_root->left == NULL && external_root->right != NULL)
-			{
-				external_root = external_root->right;
-				return;
-			}
-			else if (external_root->left != NULL && external_root->right != NULL)
-			{
-				Binary_tree_node* prev = external_root->left;
-				Binary_tree_node* p_prev = external_root;
-				while (prev->right != NULL)
-				{
-					p_prev = prev;
-					prev = prev->right;
-				}
-				external_root->data = prev->data;
-				if (external_root->left == prev)
-				{
-					external_root->left = prev->left;
-					delete prev;
-					return;
-				}
-				p_prev->right = prev->left;
-				delete prev;
-				return;
-			}
-		}
-	}
-	void Binary_tree_delete_node(int data)
-	{
-		Binary_tree_delete_node_helper(data, root);
-	}
-	void Binary_tree_search_helper(int target, Binary_tree_node*& external_root, int& output, bool& get_result)
-	{
-		if (target > external_root->data && external_root->right != NULL)
-		{
-			output++;
-			Binary_tree_search_helper(target, external_root->right, output, get_result);
-		}
-		else if (target < external_root->data && external_root->left != NULL)
-		{
-			output++;
-			Binary_tree_search_helper(target, external_root->left, output, get_result);
-		}
-		else if (target == external_root->data)
-		{
-			output++;
-			get_result = true;
-			return;
-		}
-		return;
-	}
-	int Binary_tree_search(int target)
+	if (index == substr.size())
 	{
 		int result = 0;
-		bool get_result = false;
-		Binary_tree_search_helper(target, root, result, get_result);
-		if (get_result)
-			return result;
-		else
-			return -1;
-	}
-	void Binary_tree_build(vector<int> datas)
-	{
-		int size = datas.size();
-		for (int i = 0; i < size; i++)
-			Binary_tree_insert(datas[i]);
+		DFS(root, result);
+		cout << result << endl;
+		flag = 1;
 		return;
 	}
-};
+	for (unsigned i = 0;i < root->child.size();i++)
+		if (root->child[i]->data == substr[index])
+		{
+			Search_helper(root->child[i], index + 1);
+			return;
+		}
+	if(flag == 0)
+		cout << 0 << endl;
+}
+void trie::Search()
+{
+	flag = 0;
+	Search_helper(this, 0);
+}
 int main()
 {
+	trie* root = new trie('#');
+	while (cin >> str)
+	{
+		root->Insert();
+		if (cin.get() == '\n')
+			break;
+	}
+	root->Level_traversal();
+	cout << endl;
 	int t;
 	cin >> t;
 	while (t--)
 	{
-		int num;
-		cin >> num;
-		vector<int>datas = vector<int>(num);
-		for (int i = 0; i < num; i++)
-			cin >> datas[i];
-		cin >> num;
-		Binary_tree* tree = new Binary_tree();
-		tree->Binary_tree_build(datas);
-		tree->Binary_tree_inorder_traversal(num - 1);
+		cin >> substr;
+		root->Search();
 	}
 	return 0;
 }
-/**********************************************************************
-	Problem: 1166
-	User: 202200201118
-	Language: C++
-	Result: AC
-	Time:12 ms
-	Memory:2224 kb
-**********************************************************************/
--- 数据结构13--哈希查找&&排序/E：DS内排—直插排序/AC.cpp
+++ 数据结构13--哈希查找&&排序/E：DS内排—直插排序/AC.cpp
@@ -1,65 +1,42 @@
 #include <iostream>
 using namespace std;
-class Binary_tree
+int BubbleSort(int *list, int length)
 {
-public:
-	char* datas;
-	int data_size;
-
-	Binary_tree() : datas(NULL), data_size(0) {}
-	Binary_tree(int size)
+	int count = 0;
+	for(int i = 0;i < length;i++)
 	{
-		data_size = size;
-		datas = new char[data_size];
-	}
-	Binary_tree(char* external_datas, int size)
-	{
-		data_size = size;
-		datas = external_datas;
-	}
-	int BalanceFactor_helper(int index)
-	{
-		if (datas[index] == '0' || index >= data_size)
+		for(int j = i;j < length;j++)
 		{
-			return 0;
+			if(list[i] > list[j])
+			{
+				int temp = list[i];
+				list[i] = list[j];
+				list[j] = temp;
+				count++;
+			}
 		}
-		int left_level = BalanceFactor_helper(2 * index + 1);
-		int right_level = BalanceFactor_helper(2 * index + 2);
-		cout << datas[index] << ' ' << left_level - right_level << endl;
-		if (left_level > right_level)
-			return left_level + 1;
-		else
-			return right_level + 1;
 	}
-	void BalanceFactor()
-	{
-		char* output_char = new char[data_size];
-		int* output_int = new int[data_size];
-		int start_index = 0, output_pointer = 0;
-		BalanceFactor_helper(start_index);
-	}
-};
+	return count;
+}
 int main()
 {
 	int t;
-	cin >> t;
-	while (t--)
+	while(cin >> t)
 	{
-		int num;
-		cin >> num;
-		char* datas = new char[num];
-		for (int i = 0; i < num; i++)
-			cin >> datas[i];
-		Binary_tree* tree = new Binary_tree(datas, num);
-		tree->BalanceFactor();
+		int *list = new int[t];
+		for(int i = 0;i < t;i++)
+			cin >> list[i];
+		int result = BubbleSort(list, t);
+		cout << result << endl;
 	}
 	return 0;
 }
 /**********************************************************************
-	Problem: 1042
-	User: 202200201118
-	Language: C++
-	Result: AC
-	Time:10 ms
-	Memory:2224 kb
-**********************************************************************/
+  Problem: 1029
+  User: 202200201118
+  Language: C++
+  Result: AC
+  Time:75 ms
+  Memory:2220 kb
+ **********************************************************************/
+
